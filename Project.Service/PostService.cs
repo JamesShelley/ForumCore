75e7bd29b4f73849cfbf127c6bda92b5f56a57ee
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-28 10:13:11 +0000

    add return single post to post service

--- Project.Service/PostService.cs
+++ Project.Service/PostService.cs
@@ -45,7 +45,11 @@ namespace Project.Service
 
         public Post GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Posts.Where(post => post.Id == id)
+                 .Include(post => post.User)
+                 .Include(post => post.Replies).ThenInclude(reply => reply.User)
+                 .Include(post => post.Forum)
+                 .First();
         }
 
         public IEnumerable<Post> GetFilteredPosts(string searchQuery)
