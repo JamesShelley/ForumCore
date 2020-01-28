f572d2e5134601ce8acc0d4e9f0f4eceeb6c2714
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-28 08:44:49 +0000

    create post service from interface

---
+++ Project.Service/PostService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Data.Models;
+using StopGambleProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Project.Service
+{
+    public class PostService : IPost
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Add(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddReply(PostReply reply)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPostCotnent(int id, string newContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetFilteredPosts(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
