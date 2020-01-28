58cae681858c137989e6430420252af7b9af1efd
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-28 08:57:24 +0000

    database  migrations for new user columns

--- Project.Data/Migrations/20200128085559_update ApplicationUser with forum specific columns.Designer.cs
+++ Project.Data/Migrations/20200128085559_update ApplicationUser with forum specific columns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StopGambleProject.Data;
 
 namespace Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200128085559_update ApplicationUser with forum specific columns")]
+    partial class updateApplicationUserwithforumspecificcolumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
