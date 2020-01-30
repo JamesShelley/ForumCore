1efecb16f351de767f436cca30ac943ad9b5d632
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-30 02:38:11 +0000

    adding a random about us page ready for future

--- StopGambleProject/Controllers/HomeController.cs
+++ StopGambleProject/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace StopGambleProject.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
