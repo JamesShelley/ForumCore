5e5f60715665bbc296b510a1ce1efd74e08492b8
James Shelley <james.shelley@greenwoodcampbell.com>
2020-02-03 02:10:36 +0000

    change register/signin to use username instead of email address

--- StopGambleProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StopGambleProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,7 +50,12 @@ namespace StopGambleProject.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
+            
+            [Required]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+            
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
@@ -75,7 +80,7 @@ namespace StopGambleProject.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
