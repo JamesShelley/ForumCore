e7e8f545df0164b595fcf7209d71105e230fc338
James Shelley <james.shelley@greenwoodcampbell.com>
2020-02-03 02:01:23 +0000

    create dataseeder to create admin role and superuser

---
+++ Project.Data/DataSeeder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Project.Data.Models;
+using StopGambleProject.Data;
+
+namespace Project.Data
+{
+    public class DataSeeder
+    {
+        private ApplicationDbContext _context;
+
+        public DataSeeder(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task SeedSuperUser()
+        {
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            var userStore = new UserStore<ApplicationUser>(_context);
+     
+            //When converting to SaaS - change this to user values
+            var user = new ApplicationUser
+            {
+                UserName = "Admin",
+                NormalizedUserName = "admin",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var hasher = new PasswordHasher<ApplicationUser>();
+            //When converting to SaaS - change this to user values
+            var hashedPassword = hasher.HashPassword(user, "admin");
+            user.PasswordHash = hashedPassword;
+            
+            var hasAdminRole = _context.Roles.Any(roles => roles.Name == "Admin");
+        
+            if (!hasAdminRole)
+            { 
+                roleStore.CreateAsync(new IdentityRole {Name = "Admin", NormalizedName = "admin"});
+            }
+
+            var hasSuperUser = _context.Users.Any(u => u.NormalizedUserName == user.UserName);
+
+            if (!hasSuperUser)
+            {
+                 userStore.CreateAsync(user);
+                 userStore.AddToRoleAsync(user, "Admin");
+            }
+
+             _context.SaveChangesAsync();
+
+             return Task.CompletedTask;
+        }
+    }
+}
--- StopGambleProject/Startup.cs
+++ StopGambleProject/Startup.cs
@@ -46,11 +46,11 @@ namespace StopGambleProjects
             services.AddScoped<IForum, ForumService>();
             services.AddScoped<IPost, PostService>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
-
+            services.AddTransient<DataSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataSeeder dataSeeder)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +63,9 @@ namespace StopGambleProjects
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            dataSeeder.SeedSuperUser();
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
