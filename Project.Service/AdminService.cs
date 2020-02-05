26b47e8256f14d06e844549439d9a30e9d9e386a
James Shelley <james.shelley@greenwoodcampbell.com>
2020-02-05 11:51:17 +0000

    start on admin control panel + create moderator functionality in admin service

--- Project.Service/AdminService.cs
+++ Project.Service/AdminService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Project.Data;
+using Project.Data.Models;
 using StopGambleProject.Data;
 
 namespace Project.Service
@@ -7,15 +10,23 @@ namespace Project.Service
     public class AdminService : IAdmin
     {
         private readonly ApplicationDbContext _context;
+        private readonly ApplicationUserService _userService;
 
-        public AdminService(ApplicationDbContext context)
+        public AdminService(ApplicationDbContext context, ApplicationUserService userService)
         {
             _context = context;
+            _userService = userService;
         }
         
-        public Task CreateModerator(string id)
+        public async Task CreateModerator(string id)
         {
-            throw new System.NotImplementedException();
+            var userStore = new UserStore<ApplicationUser>(_context);
+            var user = _userService.GetById(id);          
+            
+            userStore.AddToRoleAsync(user, "Moderator").Wait();
+            userStore.AddToRoleAsync(user, "moderator").Wait();
+            
+            await _context.SaveChangesAsync();
         }
     }
 }
